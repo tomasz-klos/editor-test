474498dfdbc0373931f84400f2970bf660e025f8
Tomasz Kłos <xilent98@wp.pl>
2020-07-22 17:02:26 +0200

    editor construction

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,17 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+const textarea = document.querySelector('.textarea--js');
+const save = document.querySelector('.save--js');
+const load = document.querySelector('.load--js');
+
+save.addEventListener('click', (e) => {
+    e.preventDefault();
+    localStorage.setItem('entry', textarea.value);
+})
+
+load.addEventListener('click', (e) => {
+    e.preventDefault();
+    textarea.value = localStorage.getItem('entry');
+})
